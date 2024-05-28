7b12a6ba7840a904d88783bff988fe5db9f3daa7
sarathwgithub <147220538+sarathwgithub@users.noreply.github.com>
2024-05-28 12:12:47 +0530

    Create init.php

---
+++ class/init.php
@@ -0,0 +1,6 @@
+<?php
+
+include $_SERVER['DOCUMENT_ROOT'].'/test_projects/bittest_2/config.php';
+include $_SERVER['DOCUMENT_ROOT'].'/test_projects/bittest_2/function.php';
+include $_SERVER['DOCUMENT_ROOT'].'/test_projects/bittest_2/class/User.php';
+include $_SERVER['DOCUMENT_ROOT'].'/test_projects/bittest_2/class/Employee.php';
