221a28a1ee32ac6d3c6d73cc5f8416498e355e92
sarathwgithub <147220538+sarathwgithub@users.noreply.github.com>
2024-06-04 13:41:37 +0530

    Update Employee.php

--- class/Employee.php
+++ class/Employee.php
@@ -6,9 +6,9 @@ class Employee extends User {
     public function __construct() {
         parent::__construct();  // Call the parent constructor to initialize the database connection
     }    
-    public function insertEmployee($FirstName, $LastName, $UserName, $Password, $AppDate, $DesignationId, $DepartmentId) {
+    public function save($FirstName=null, $LastName=null, $UserName=null, $Password=null, $AppDate=null, $DesignationId=null, $DepartmentId=null) {
         // Insert user and get the UserId
-        $UserId = $this->insertUser($FirstName, $LastName, $UserName, $Password, 'employee', '1');
+        $UserId = parent::save($FirstName, $LastName, $UserName, $Password, 'employee', '1');
 
         // Insert into employee table
         $sql = "INSERT INTO employee (AppDate, DesignationId, DepartmentId, UserId) 
